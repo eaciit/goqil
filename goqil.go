750a86c610804080985d9ec5c0e87573ffc191c9
juragan360 <ariefda@hotmail.com>
2016-03-08 18:33:12 +0700

    add doc

--- goqil.go
+++ goqil.go
@@ -1,5 +1,9 @@
 package goqil
 
+/*Parse is used to apply JQuery selector into html string. 
+This method will return array of string that matches the selector. 
+Selector also support enhanced goqil selector as below:
+*/
 func Parse(html string, selector string) []string{
     return []string{}
 }
