16ecdb0d7aa892e5b79e9ba73da326c16f2294ce
juragan360 <ariefda@hotmail.com>
2016-03-08 17:38:03 +0700

    init

---
+++ goqil.go
@@ -0,0 +1,5 @@
+package goqil
+
+func Parse(html string, selector string) []string{
+    return []string{}
+}
---
+++ tests/goqil_test.go
@@ -0,0 +1,9 @@
+package tests
+
+import (
+    "testing"
+)
+
+func TestGoqil(t *testing.T){
+    
+}
